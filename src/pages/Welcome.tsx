3428f71c1353e2796a23f215c9487bf019843851
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-28 23:07:44 -0300

    Create Welcome page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import wateringImage from "../assets/watering.png";
+import colors from "../styles/colors";
+
+export function Welcome() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Manage your plants {'\n'} easily
+            </Text>
+
+            <Image source={wateringImage} style={styles.image} />
+
+            <Text style={styles.subtitle}>
+                Don't forget to water your plants anymore. We take care to remind you whenever you need it.
+            </Text>
+
+            <TouchableOpacity 
+                style={styles.button}
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>
+                    >
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: colors.heading,
+        marginTop: 38
+    },
+    image: {
+        width: 292,
+        height: 284
+    },
+    subtitle: {
+        textAlign: "center",
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+});
