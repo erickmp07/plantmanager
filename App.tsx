d736d7198771bc7cf02f5909624ea9e050fbcb11
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-12-08 14:37:58 -0300

    Set routes created to the app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppLoading from "expo-app-loading";
+
+import Routes from "./src/routes";
+
 import {
     useFonts,
     Jost_400Regular,
     Jost_600SemiBold
 } from "@expo-google-fonts/jost";
-import { Welcome } from "./src/pages/Welcome";
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -20,6 +22,6 @@ export default function App() {
     }
 
     return (
-        <Welcome />
+        <Routes />
     );
 }
